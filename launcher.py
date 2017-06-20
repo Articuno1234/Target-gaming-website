e8691c5fe1e0e8808b02a936696e123cd66ab942
Articuno1234 <Articuno1234@users.noreply.github.com>
2017-06-20 19:45:30 +1000

    Update launcher.py

--- launcher.py
+++ launcher.py
@@ -1,1 +1,15 @@
+sys = True
+try:
+  import sys
+else:
+  print("sys was not found! running any way :)")
+  sys = False
+import os
 
+def nosys():
+  print("=Target gaming========================|==Staff|==Games==|==Info==\n"
+        "                                                                 \n"
+        "                                                                 \n"                                                                 \n"
+        "       Target gaming is a small game design studio               \n"
+        "                                                                 \n"
+        "                   More comming soon!                            \n")
